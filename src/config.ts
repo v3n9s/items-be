3bab214584c5ebce82289eed525ad1df3e221a3f
Venyamin Shvets <venyashvets@gmail.com>
2022-08-16 19:33:41 +0300

    feat: update config

--- src/config.ts
+++ src/config.ts
@@ -4,17 +4,39 @@ import { Joi } from 'express-validation';
 interface Config {
   DATABASE_URL: string;
   PORT: number;
+  JWT_ACCESS_SECRET_KEY: string;
+  JWT_ACCESS_EXPIRE_HOURS: number;
+  JWT_ACCESS_EXPIRE_MS: number;
+  JWT_REFRESH_SECRET_KEY: string;
+  JWT_REFRESH_EXPIRE_DAYS: number;
+  JWT_REFRESH_EXPIRE_MS: number;
 }
 
-const schema = Joi.object<Config>({
+const schema = Joi.object<Readonly<Config>>({
   DATABASE_URL: Joi.string(),
-  PORT: Joi.number().integer()
+  PORT: Joi.number().integer(),
+  JWT_ACCESS_SECRET_KEY: Joi.string(),
+  JWT_ACCESS_EXPIRE_HOURS: Joi.number(),
+  JWT_ACCESS_EXPIRE_MS: Joi.number(),
+  JWT_REFRESH_SECRET_KEY: Joi.string(),
+  JWT_REFRESH_EXPIRE_DAYS: Joi.number(),
+  JWT_REFRESH_EXPIRE_MS: Joi.number()
 });
 
 const config = schema.validate({
   DATABASE_URL: process.env.DATABASE_URL,
-  PORT: process.env.PORT
-}, {
+  PORT: process.env.PORT,
+  JWT_ACCESS_SECRET_KEY: process.env.JWT_ACCESS_SECRET_KEY,
+  JWT_ACCESS_EXPIRE_HOURS: process.env.JWT_ACCESS_EXPIRE_HOURS,
+  get JWT_ACCESS_EXPIRE_MS() {
+    return this.JWT_ACCESS_EXPIRE_HOURS * 60 * 60 * 1000;
+  },
+  JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
+  JWT_REFRESH_EXPIRE_DAYS: process.env.JWT_REFRESH_EXPIRE_DAYS,
+  get JWT_REFRESH_EXPIRE_MS() {
+    return this.JWT_REFRESH_EXPIRE_DAYS * 24 * 60 * 60 * 1000;
+  }
+} as {[key in keyof Config]: any}, {
   abortEarly: false,
   presence: 'required'
 });
