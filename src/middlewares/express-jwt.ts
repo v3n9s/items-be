1aa1d0eea82a3914488560a26100c82ec4f6ce26
Venyamin Shvets <venyashvets@gmail.com>
2022-08-16 22:52:05 +0300

    feat: create wrapper for express-jwt middleware

--- src/middlewares/express-jwt.ts
+++ src/middlewares/express-jwt.ts
@@ -1,5 +1,16 @@
 import { ErrorRequestHandler } from 'express';
-import { UnauthorizedError } from 'express-jwt';
+import { expressjwt, UnauthorizedError } from 'express-jwt';
+import config from '../config';
+
+export const customExpressJwt = (
+  options?: Partial<Parameters<typeof expressjwt>['0']>
+) => {
+  return expressjwt({
+    algorithms: ['HS256'],
+    secret: config.JWT_ACCESS_SECRET_KEY,
+    ...options
+  });
+}
 
 export const unauthorizedErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (err instanceof UnauthorizedError) {
