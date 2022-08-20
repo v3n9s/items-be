da095e041f2d280dd55ba0e12acf3a3b7338b65a
Venyamin Shvets <venyashvets@gmail.com>
2022-08-20 13:17:42 +0300

    feat: update item and tag entities

--- src/entities/item.ts
+++ src/entities/item.ts
@@ -1,4 +1,11 @@
-import { Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn
+} from 'typeorm';
 import { Collection } from './collection';
 import { Tag } from './tag';
 
@@ -7,7 +14,11 @@ export class Item {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
+  name: string;
+
   @ManyToMany(() => Tag, (tag) => tag.items)
+  @JoinTable()
   tags: Tag[];
 
   @ManyToOne(() => Collection, (collection) => collection.items)
--- src/entities/tag.ts
+++ src/entities/tag.ts
@@ -1,10 +1,15 @@
-import { Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Item } from './item';
 
 @Entity('Tags')
 export class Tag {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({
+    unique: true
+  })
+  name: string;
 
   @ManyToMany(() => Item, (item) => item.tags)
   items: Item[];
